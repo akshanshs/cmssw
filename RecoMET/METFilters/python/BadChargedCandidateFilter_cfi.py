de030074d39ac415ff2acc6d92ec84926efcb105
Akshansh Singh <akshansh.singh@desy.de>
2016-09-27 17:17:00 +0200

    Filter update

--- RecoMET/METFilters/python/BadChargedCandidateFilter_cfi.py
+++ RecoMET/METFilters/python/BadChargedCandidateFilter_cfi.py
@@ -6,9 +6,9 @@ BadChargedCandidateFilter = cms.EDFilter(
     muons  = cms.InputTag("muons"),   # Collection to test
     taggingMode   = cms.bool(False),
     debug         = cms.bool(False),
-    maxDR         = cms.double(0.001),              # Maximum DR between reco::muon->innerTrack and pfCandidate 
-    minPtDiffRel = cms.double(-0.5),               # lower threshold on difference between pt of reco::muon->innerTrack and pfCandidate
+    maxDR         = cms.double(0.00001),              # Maximum DR between reco::muon->innerTrack and pfCandidate 
+    minPtDiffRel = cms.double(0.00001),               # lower threshold on difference between pt of reco::muon->innerTrack and pfCandidate
                                                    # computed as (pfcand.pt - muon.track.pt)/(0.5*(pfcand.pt + muon.track.pt))
     minMuonTrackRelErr = cms.double(0.5),          # minimum ptError/pt on muon innertrack 
-    minMuonPt     = cms.double(100),               # minimum muon pt 
+    minMuonPt     = cms.double(100.0),               # minimum muon pt 
 )
